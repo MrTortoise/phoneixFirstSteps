afd254e1a54aac0d0291cf7ede3380db1b16480f
John Nicholas <themrtortoise@gmail.com>
2017-04-15 23:14:18 +0100

    user create works. Also fixed bug in validation code since book was written

--- projectWithUsers/rumbl/web/models/user.ex
+++ projectWithUsers/rumbl/web/models/user.ex
@@ -9,4 +9,12 @@ defmodule Rumbl.User do
 
     timestamps()
   end
+
+  def changeset(model, params \\ :invalid) do
+    model
+    |> cast(params, ~w(name username))
+    |> validate_required([:name, :username ])
+    |> validate_length(:username, min: 1, max: 20)
+
+  end
 end
