6d0c13984fa0f10f2c57e8495118ea3a586aeee6
John Nicholas <themrtortoise@gmail.com>
2017-06-28 19:39:47 +0100

    added videos

--- projectWithUsers/rumbl/web/controllers/auth.ex
+++ projectWithUsers/rumbl/web/controllers/auth.ex
@@ -43,7 +43,7 @@ defmodule Rumbl.Auth do
     |> configure_session(drop: true)
   end
 
-  def authenticate(conn, _opts) do
+  def authenticate_user(conn, _opts) do
      if conn.assigns.current_user do
        conn
      else
